3663fc86a7f96f692f4cfe031182ddd0b4f81b71
Naufal Rafi <naufalrafiar@gmail.com>
2024-11-07 16:39:22 +1000

    fix: auth0 middleware redirect after login

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,7 @@
 // src/middleware.ts
 import { withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge';
 
-export default withMiddlewareAuthRequired({
-  returnTo: '/login',
-});
+export default withMiddlewareAuthRequired();
 
 export const config = {
   matcher: [
